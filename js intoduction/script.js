0892c91265ae26245cd224328ad9fd7e61dcffbf
trial-alhazen <trial0002@alhazen.academy>
2024-10-11 08:52:59 +0700

    Add files via upload

---
+++ js intoduction/script.js
@@ -0,0 +1,11 @@
+// alert = sebuah function untuk menampilkan pop up
+alert("selamat datang");
+alert(" ini adalah alert");
+
+// prompt = sebuah function untuk menerima input dari user
+prompt("siapa nama kamu ? ");
+
+
+
+
+
