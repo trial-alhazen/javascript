0dc382be14c077bd0a7c7e8a65f9a3b3dc9268b2
trial-alhazen <trial0002@alhazen.academy>
2024-10-18 14:07:51 +0700

    Add files via upload

--- js intoduction/script.js
+++ js intoduction/script.js
@@ -1,11 +1,59 @@
-// alert = sebuah function untuk menampilkan pop up
-alert("selamat datang");
-alert(" ini adalah alert");
+// // // alert = sebuah function untuk menampilkan pop up
+// // alert("selamat datang");
+// // alert(" ini adalah alert");
 
-// prompt = sebuah function untuk menerima input dari user
-prompt("siapa nama kamu ? ");
+// // // prompt = sebuah function untuk menerima input dari user
+// // prompt("siapa nama kamu ? ");
+
+// // Variable Var
+// // deskripsi awal
+// var nama = "ahmad";
+// console.log(nama);
+// // diubah
+// nama = "budi"
+// console.log(nama);
+// // deskripsi ulang
+// var nama = "asep"
+// console.log(nama);
+
+// // Variable let
+// // deskripsi awal
+// let umur = 20;
+// console.log(umur);
+// // diubah
+// umur = 31
+// console.log(umur);
+// // tidak bisa deskripsi ulang
+// // let umur = 25
+// // console.log(umur);
+
+// // Variable const
+// // deskripsi awal
+// const alamat = "bogor";
+// console.log(alamat);
+// //tidak bisa diubah
+// // alamat = "depok";
+// // console.log(alamat);
+// // tidak bisa deskripsi ulang
+// // let umur = 25
+// // console.log(umur);
 
 
+// console.log(6+2);
+
+// angka1 = 13;
+// angka2 = 10;
+// hasil = angka1 * angka2;
+// console.log(hasil);
 
 
+// membuat kalkulator sederhana
+let number1 = prompt("masukan angka pertama : ");
+let number2 = prompt("masukan angka kedua : ");
+let result = parseInt(number1) + parseInt(number2);
+alert(result);
 
+// Array
+fruit=["banana", "apple","orange","durian"];
+
+alert(fruit[2]);
